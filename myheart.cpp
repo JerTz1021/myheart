4dfd78d93df7cf2c82c974383bfadce7cf16a9cc
JerTz1021 <jeremytzib@gmail.com>
2026-03-02 21:03:29 -0600

    Add files via upload

---
+++ myheart.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+struct myHeart {
+    int ourNumber;
+    string fact = "You and I Will Always Be With Each Other";
+    string shatteredHeart = " I would be broken without you";
+    string MyHeart = "I Love You, You complete me, You Will Alwyas Be A Part Of My Heart,";
+    string notFact = "Wha do you mean Error?, We Ain't Ever Lettin Go Of Each Other :3";
+};
+
+int main () {
+    myHeart Heart;
+
+    cout << Heart.fact << endl;
+
+    cout << "Enter 2110 or 404:" << endl;
+    cin >> Heart.ourNumber;
+    
+    
+    if(Heart.ourNumber == 2110) {
+        cout << Heart.MyHeart << Heart.shatteredHeart << endl;
+    } else if (Heart.ourNumber = 404) {
+        cout << Heart.notFact << endl;
+    } else {
+        cout << "What the fuck are you typin" << endl;
+    }
+}
